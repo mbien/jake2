e1d570aecf4e6d9ee17cf35852b1431e6c650f44
Carsten Weisse <cwei@bytonic.de>
2005-05-20 16:58:06 +0000

    bugfix: wrong path

--- scripts/Jake2_lwjgl.sh
+++ scripts/Jake2_lwjgl.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=lib/linux/lwjgl
 CP=lib/jake2.jar:lib/lwjgl.jar:lib/lwjgl_util.jar
 
-exec java -Xmx80M -Djava.library.path=lib/linux -cp $CP jake2.Jake2
+exec java -Xmx80M -Djava.library.path=lib/linux/lwjgl -cp $CP jake2.Jake2
