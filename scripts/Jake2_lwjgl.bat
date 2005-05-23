2d52e0a56baa8d81c1749cc2d009dda7af8d20d9
Holger Zickner <hoz@bytonic.de>
2005-05-23 06:20:06 +0000

    use 100MB heap size

--- scripts/Jake2_lwjgl.bat
+++ scripts/Jake2_lwjgl.bat
@@ -1,4 +1,4 @@
 @echo off
 SET PATH=lib\windows;%PATH%
 SET CP=lib/jake2.jar;lib/lwjgl.jar;lib/lwjgl_util.jar
-java -Xmx80M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp %CP% jake2.Jake2
+java -Xmx100M -Dsun.java2d.noddraw=true -Djava.library.path=lib/windows -cp %CP% jake2.Jake2
--- scripts/Jake2_mac_lwjgl.sh
+++ scripts/Jake2_mac_lwjgl.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=lib/linux/osx
 CP=lib/jake2.jar:lib/lwjgl.jar:lib/lwjgl_util.jar
 
-exec java -Xmx80M -Djava.library.path=lib/osx -cp $CP jake2.Jake2
+exec java -Xmx100M -Djava.library.path=lib/osx -cp $CP jake2.Jake2
